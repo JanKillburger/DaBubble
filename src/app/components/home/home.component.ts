85cdbfbf1b54b0a252f29c92e64401e6eac7cd9e
Jan Killburger <jkillburger@gmail.com>
2024-02-03 19:16:38 +0100

    added medium screen query

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectorRef, Component, ElementRef, ViewChild } from '@angular/core';
+import { NgIf } from '@angular/common';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { ExampleDialogComponent } from '../example-dialog/example-dialog.component';
@@ -6,23 +7,42 @@ import { ExampleDialogComponent } from '../example-dialog/example-dialog.compone
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatDialogModule],
+  imports: [MatDialogModule, NgIf],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  screenMode: "small" | "medium" | "large" = "large";
   navVisible = true;
   threadVisible = true;
-  mdq: MediaQueryList;
+  threadVisibleMq = true;
+  channelVisible = true;
+  mqlMaxWidth: MediaQueryList;
+  mqlMediumWidth: MediaQueryList;
   @ViewChild('dialogTrigger') dialogTrigger!: ElementRef;
 
   constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, public dialog: MatDialog) {
-    this.mdq = media.matchMedia('(min-width: 1440px)');
-    this.mdq.onchange = (event) => {
-      changeDetectorRef.detectChanges();
+    this.mqlMaxWidth = media.matchMedia('(min-width: 1440px)');
+    this.mqlMaxWidth.onchange = (event) => {
       if (event.matches) {
         this.navVisible = true;
       }
+      changeDetectorRef.detectChanges();
+    };
+    this.mqlMediumWidth = media.matchMedia('(min-width: 992px)');
+    this.threadVisibleMq = this.mqlMediumWidth.matches;
+    this.mqlMediumWidth.onchange = (event) => {
+      console.log(event.matches);
+      if (event.matches) {
+        this.screenMode = "large";
+        this.channelVisible = true;
+        this.threadVisibleMq = true;
+      } else {
+        this.screenMode = "medium";
+        this.channelVisible = true;
+        this.threadVisibleMq = false;
+      }
+      changeDetectorRef.detectChanges();
     }
   }
   toggleNav() {
@@ -31,10 +51,15 @@ export class HomeComponent {
 
   closeThread() {
     this.threadVisible = false;
+    this.channelVisible = true;
   }
 
   openThread() {
+    this.threadVisibleMq = true;
     this.threadVisible = true;
+    if (this.screenMode == "medium") {
+      this.channelVisible = false;
+    }
     document.body.style.overflow = "hidden";
     setTimeout(() => {
       document.body.removeAttribute("style");
@@ -43,7 +68,6 @@ export class HomeComponent {
 
   openDialog() {
     const bounds = this.dialogTrigger.nativeElement.getBoundingClientRect();
-    console.log(bounds);
     this.dialog.open(ExampleDialogComponent, { position: { top: bounds.bottom + "px", left: bounds.left + "px" } })
   }
 }
