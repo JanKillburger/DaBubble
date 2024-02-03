b47cefc62950c41606751a6a03d6361b64e057a1
Jan Killburger <jkillburger@gmail.com>
2024-02-03 17:37:55 +0100

    Added working example

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectorRef, Component, ElementRef, ViewChild } from '@angular/core';
 import { MediaMatcher } from '@angular/cdk/layout';
-import { BoundElementProperty } from '@angular/compiler';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { ExampleDialogComponent } from '../example-dialog/example-dialog.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -13,8 +14,9 @@ export class HomeComponent {
   navVisible = true;
   threadVisible = true;
   mdq: MediaQueryList;
+  @ViewChild('dialogTrigger') dialogTrigger!: ElementRef;
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, public dialog: MatDialog) {
     this.mdq = media.matchMedia('(min-width: 1440px)');
     this.mdq.onchange = (event) => {
       changeDetectorRef.detectChanges();
@@ -37,5 +39,11 @@ export class HomeComponent {
     setTimeout(() => {
       document.body.removeAttribute("style");
     }, 500);
+  }
+
+  openDialog() {
+    const bounds = this.dialogTrigger.nativeElement.getBoundingClientRect();
+    console.log(bounds);
+    this.dialog.open(ExampleDialogComponent, { position: { top: bounds.bottom + "px", left: bounds.left + "px" } })
   }
 }
