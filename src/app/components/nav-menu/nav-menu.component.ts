8b90a7cc316f7a39ee5a861f91a866f88f30fc5f
Jan Killburger <jkillburger@gmail.com>
2024-02-08 08:53:43 +0100

    Added material icon fonts

--- src/app/components/nav-menu/nav-menu.component.ts
+++ src/app/components/nav-menu/nav-menu.component.ts
@@ -1,17 +1,12 @@
-import { Component, Output, EventEmitter, Input } from '@angular/core';
+import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon'; 
 
 @Component({
   selector: 'app-nav-menu',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './nav-menu.component.html',
   styleUrl: './nav-menu.component.scss'
 })
 export class NavMenuComponent {
-  @Input() navVisible!: boolean;
-  @Output() toggleNavEvent = new EventEmitter<void>();
-
-  toggleNav(): void {
-    this.toggleNavEvent.emit();
-  }
 }
