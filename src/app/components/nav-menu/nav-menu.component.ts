7f11b9d8683b36a35c8f7c37af0d2200fb4a9ed0
Jan Killburger <jkillburger@gmail.com>
2024-02-08 20:53:42 +0100

    Added Nunito font by link

--- src/app/components/nav-menu/nav-menu.component.ts
+++ src/app/components/nav-menu/nav-menu.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon'; 
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-nav-menu',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, MatButtonModule],
   templateUrl: './nav-menu.component.html',
   styleUrl: './nav-menu.component.scss'
 })
