522eabbcf715d0d1f7a90b49fd63b7afa39af219
Jan Killburger <jkillburger@gmail.com>
2024-02-09 11:49:57 +0100

    Added channel and messages dropdowns

--- src/app/components/nav-menu/nav-menu.component.ts
+++ src/app/components/nav-menu/nav-menu.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-nav-menu',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule],
+  imports: [MatIconModule, MatButtonModule, NgIf],
   templateUrl: './nav-menu.component.html',
   styleUrl: './nav-menu.component.scss'
 })
 export class NavMenuComponent {
+  messagesExpanded = true;
+  channelsExpanded = true;
+
+  toggleMessages() {
+    this.messagesExpanded = !this.messagesExpanded;
+  }
+
+  toggleChannels() {
+    this.channelsExpanded = !this.channelsExpanded;
+  }
 }
