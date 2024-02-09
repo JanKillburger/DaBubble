7aae43774abb98808231429e1cd3168a8ee1a959
Max Wirnsberger <maximilian.wirnsberger@gmail.com>
2024-02-09 14:09:58 +0100

    created login with firebase function

--- src/app/components/login/login-dialog/login-dialog.component.ts
+++ src/app/components/login/login-dialog/login-dialog.component.ts
@@ -1,15 +1,30 @@
 import { CommonModule, NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { User } from '../../../models/user.class';
+import { FirebaseAuthService } from '../../../services/firebase-auth.service';
 
 @Component({
   selector: 'app-login-dialog',
   standalone: true,
   imports: [NgIf, NgClass, CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './login-dialog.component.html',
-  styleUrl: './login-dialog.component.scss'
+  styleUrl: './login-dialog.component.scss',
 })
 export class LoginDialogComponent {
+  // rightUser: boolean;
+  // rightPassword: boolean;
+  loading: boolean = false;
+  user = new User();
+
+  constructor(public authService: FirebaseAuthService) {}
+
   logInForm: FormGroup = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [
@@ -18,24 +33,40 @@ export class LoginDialogComponent {
     ]),
   });
 
-  get email() {return this.logInForm.get("email")}
-  get password() {return this.logInForm.get("password")}
-
-  loginFunction(){
-    
+  get email() {
+    return this.logInForm.get('email');
+  }
+  get password() {
+    return this.logInForm.get('password');
   }
 
-  guestLogin(){
-    
+  loginFunction() {
+    this.authService.loginWithEmailAndPassword(
+      this.user.email,
+      this.user.password
+    );
   }
 
-  forgotPasswort(){
-    let contactButton = document.getElementById('create-contact-button')
-    let forgotPasswortDialog = document.getElementById('forgot-passwort-dialog')
-    let loginDialog = document.getElementById('login-dialog')
-    
-    forgotPasswortDialog?.classList.remove('display_none')
-    contactButton?.classList.add('display_none')
-    loginDialog?.classList.add('display_none')
+  guestLogin() {}
+
+  forgotPasswort() {
+    let contactButton = document.getElementById('create-contact-button');
+    let forgotPasswortDialog = document.getElementById(
+      'forgot-passwort-dialog'
+    );
+    let loginDialog = document.getElementById('login-dialog');
+
+    forgotPasswortDialog?.classList.remove('display_none');
+    contactButton?.classList.add('display_none');
+    loginDialog?.classList.add('display_none');
   }
+
+  // async updateOnlineStatus() {
+  //   this.authService.user.onlineStatus = true;
+
+  //   await this.authService.newUser(
+  //     this.authService.user.toJson(),
+  //     this.authService.user.userId
+  //   );
+  // }
 }
