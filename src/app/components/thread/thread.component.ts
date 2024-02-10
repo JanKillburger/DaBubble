4c16b2d13e827fe3bed6f91ccd239fb133795dd2
Jan Killburger <jkillburger@gmail.com>
2024-02-10 17:35:30 +0100

    Created header

--- src/app/components/thread/thread.component.ts
+++ src/app/components/thread/thread.component.ts
@@ -1,16 +1,18 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-thread',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatButtonModule],
   templateUrl: './thread.component.html',
   styleUrl: './thread.component.scss'
 })
 export class ThreadComponent {
-@Output() closeThreadEv = new EventEmitter<void>;
+  @Output() closeThreadEv = new EventEmitter<void>;
 
-closeThread() {
-  this.closeThreadEv.emit();
-}
+  closeThread() {
+    this.closeThreadEv.emit();
+  }
 }
