1bb9543150a3ebb0f2ae111df32e1be52c10e1cf
Jan Killburger <jkillburger@gmail.com>
2024-02-11 15:24:00 +0100

    Added fake team member avatars on channel header

--- src/app/components/channel/channel.component.ts
+++ src/app/components/channel/channel.component.ts
@@ -3,16 +3,22 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MessageComponent } from '../message/message.component';
 import { MessagesContainerComponent } from '../messages-container/messages-container.component';
+import { NgFor, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-channel',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MessageComponent, MessagesContainerComponent],
+  imports: [MatIconModule, MatButtonModule, MessageComponent, MessagesContainerComponent, NgFor, NgStyle],
   templateUrl: './channel.component.html',
   styleUrl: './channel.component.scss'
 })
 export class ChannelComponent {
   @Output() openThreadEv = new EventEmitter<void>();
+  teamMembers = [
+    'assets/img/login/SingIn/avatar1.png',
+    'assets/img/login/SingIn/avatar2.png',
+    'assets/img/login/SingIn/avatar3.png',
+  ]
 
   openThread(ev: string) {
     this.openThreadEv.emit();
