79b0d256118b8da4a62a37b3a6b4b4ae5c19d7ce
Jan Killburger <jkillburger@gmail.com>
2024-02-11 01:02:17 +0100

    Added message stylings and input properties

--- src/app/components/message/message.component.ts
+++ src/app/components/message/message.component.ts
@@ -1,13 +1,16 @@
-import { NgClass } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-message',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, NgIf, MatIconModule, MatButtonModule],
   templateUrl: './message.component.html',
   styleUrl: './message.component.scss'
 })
 export class MessageComponent {
-@Input() isMyMessage!: boolean;
+  @Input() isMyMessage!: boolean;
+  @Input() showReplies = false;
 }
