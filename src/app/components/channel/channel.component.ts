fa2e82eaffd9728e353618a4c6f0566a2869ebf7
Jan Killburger <jkillburger@gmail.com>
2024-02-11 07:26:50 +0100

    Added channel header

--- src/app/components/channel/channel.component.ts
+++ src/app/components/channel/channel.component.ts
@@ -1,16 +1,18 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-channel',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatButtonModule],
   templateUrl: './channel.component.html',
   styleUrl: './channel.component.scss'
 })
 export class ChannelComponent {
-@Output() openThreadEv = new EventEmitter<void>();
+  @Output() openThreadEv = new EventEmitter<void>();
 
-openThread() {
-  this.openThreadEv.emit();
-}
+  openThread() {
+    this.openThreadEv.emit();
+  }
 }
