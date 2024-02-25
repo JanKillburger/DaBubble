210a80db558f019d9f10d206b365b62940830f78
Max Wirnsberger <maximilian.wirnsberger@gmail.com>
2024-02-25 12:52:01 +0100

    added emojis

--- src/app/components/messages-input/messages-input.component.ts
+++ src/app/components/messages-input/messages-input.component.ts
@@ -1,15 +1,42 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { PickerModule } from '@ctrl/ngx-emoji-mart';
 
 @Component({
   selector: 'app-messages-input',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, MatIconModule, MatButtonModule],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatButtonModule,
+    PickerModule,
+    NgIf
+  ],
   templateUrl: './messages-input.component.html',
   styleUrl: './messages-input.component.scss',
 })
 export class MessagesInputComponent {
-  messageForm: FormGroup = new FormGroup({
-})}
+  showEmojiPicker = false;
+  message = '';
+  messageForm: FormGroup = new FormGroup({});
+
+  toggleEmojiPicker() {
+    console.log(this.showEmojiPicker);
+    this.showEmojiPicker = !this.showEmojiPicker;
+  }
+
+  addEmoji(event: any) {
+    console.log(this.message);
+    const { message } = this;
+    console.log(message);
+    console.log(`${event.emoji.native}`);
+    const text = `${message}${event.emoji.native}`;
+
+    this.message = text;
+    // this.showEmojiPicker = false;
+  }
+}
