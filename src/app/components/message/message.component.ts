ca1d8b1e406d33d80bd927f6e732cb79612f9ddf
Jan Killburger <jkillburger@gmail.com>
2024-02-28 22:06:10 +0100

    updated reply section in message component

--- src/app/components/message/message.component.ts
+++ src/app/components/message/message.component.ts
@@ -18,6 +18,7 @@ export class MessageComponent {
   @Input() message?: Message;
   @Input() isMyMessage!: boolean;
   @Input() showReplies = false;
+  @Input() container: "thread" | "channel" = "thread";
   @Output() openThreadEv = new EventEmitter<string>;
 
   constructor(
@@ -34,15 +35,38 @@ export class MessageComponent {
   }
 
   getUser(): UserData | undefined {
-   return this.message?.from ? this.channelService.users.get(this.message.from) : undefined;
+    return this.message?.from ? this.channelService.users.get(this.message.from) : undefined;
   }
 
-  getFormattedDay() {
-    if (this.message?.created) {
-      const date = new Date(this.message.created).toLocaleDateString('de-DE', {day: "2-digit", month: "2-digit", year: "2-digit"});
-      const time = new Date(this.message.created).toLocaleTimeString('de-DE', {hour: "2-digit", minute: "2-digit"});
-      return `${date} ${time}`
+  getFormattedDay(date: Date | undefined) {
+    if (date) {
+      const dateSection = new Date(date).toLocaleDateString('de-DE', { day: "2-digit", month: "2-digit", year: "2-digit" });
+      const timeSection = new Date(date).toLocaleTimeString('de-DE', { hour: "2-digit", minute: "2-digit" });
+      return `${dateSection} ${timeSection}`
     }
     return undefined;
   }
+
+  getReplies(): Message[] | undefined {
+    if (this.message?.id) {
+      return this.channelService.replies.get(this.message?.id)
+    } else {
+      return undefined
+    }
+  }
+
+  getLastReplyDate() {
+    const lastReply = this.getReplies()?.at(-1);
+    return lastReply ? this.getFormattedDay(lastReply.created) : undefined;
+  }
+
+  hasReplies() {
+    const replies = this.getReplies();
+    if (replies !== undefined) {
+      return replies.length > 0;
+    } else {
+      return false;
+    }
+  }
+
 }
