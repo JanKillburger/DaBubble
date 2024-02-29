f5050b643748d5c4b6004f5a21921a4e96664b5c
Jan Killburger <jkillburger@gmail.com>
2024-02-29 09:36:58 +0100

    Moved logic from home component to home service

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NgClass, NgIf } from '@angular/common';
-import { BreakpointObserver } from '@angular/cdk/layout';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { UserDialogComponent } from '../dialog-components/user-dialog/user-dialog.component';
 import { ViewportService } from '../../services/viewport.service';
@@ -8,11 +7,7 @@ import { NavMenuComponent } from '../nav-menu/nav-menu.component';
 import { ChannelComponent } from '../channel/channel.component';
 import { ThreadComponent } from '../thread/thread.component';
 import { MatIconModule } from '@angular/material/icon';
-import { MAX_INLINE_WIDTH, LARGE_WIDTH, MEDIUM_WIDTH, SMALL_WIDTH } from '../../../global-constants';
 import { UsersToChannelComponent } from '../dialog-components/users-to-channel/users-to-channel.component';
-import { Subscription } from 'rxjs';
-import { ChannelData, FirebaseChannelService, Message, messages } from '../../services/firebase-channel.service';
-import { channel } from 'diagnostics_channel';
 import { HomeService } from '../../services/home.service';
 
 @Component({
@@ -23,90 +18,15 @@ import { HomeService } from '../../services/home.service';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  screenMode: "small" | "medium" | "large" = "large";
-  navVisible = true;
-  threadVisible = true;
-  threadVisibleMq!: boolean;
-  channelVisible!: boolean;
+  
   @ViewChild('dialogTrigger') dialogTrigger!: ElementRef;
   @ViewChild('triggerUserDialog') triggerUserDialog!: ElementRef;
-  private breakpointSubscription!: Subscription;
   channels: any[] = [];
-
-
-  selectedChannelId = "yVkv2vilL4lVvya74f9Z";
 
   constructor(
     public dialog: MatDialog,
     public viewport: ViewportService,
-    private responsive: BreakpointObserver,
-    private channelService: FirebaseChannelService,
     private homeService: HomeService) { }
-
-  ngOnInit() {
-    this.breakpointSubscription = this.responsive.observe([
-      SMALL_WIDTH,
-      MEDIUM_WIDTH,
-      LARGE_WIDTH
-    ])
-      .subscribe(() => {
-
-        if (this.responsive.isMatched(SMALL_WIDTH)) {
-          this.screenMode = "small";
-          if (this.navVisible) {
-            this.channelVisible = false;
-            this.threadVisibleMq = false;
-          }
-        } else if (this.responsive.isMatched(MEDIUM_WIDTH)) {
-          this.screenMode = "medium";
-          if (this.navVisible) {
-            this.channelVisible = true;
-            this.threadVisibleMq = false;
-          }
-        } else if (this.responsive.isMatched(LARGE_WIDTH)) {
-          this.screenMode = "large";
-          this.threadVisibleMq = true;
-          this.channelVisible = true;
-        }
-      });
-  }
-
-  ngOnDestroy() {
-    this.breakpointSubscription.unsubscribe();
-  }
-
-  toggleNav() {
-    if (this.screenMode == "small") {
-      this.channelVisible = false;
-      this.threadVisibleMq = false;
-    }
-    this.navVisible = !this.navVisible;
-  }
-
-  closeThread() {
-    if (this.screenMode != "large") {
-      this.threadVisibleMq = false;
-    }
-    this.threadVisible = false;
-    this.channelVisible = true;
-  }
-
-  openThread() {
-    this.threadVisibleMq = true;
-    this.threadVisible = true;
-    if (this.screenMode != "large") {
-      this.channelVisible = false;
-    }
-    document.body.style.overflow = "hidden";
-    setTimeout(() => {
-      document.body.removeAttribute("style");
-    }, 500);
-  }
-
-  openChannel() {
-    this.navVisible = false;
-    this.channelVisible = true;
-  }
 
   openDialog() {
     const positionDetails = this.viewport.getPositionRelativeTo(this.dialogTrigger, "top", "left");
@@ -124,5 +44,33 @@ export class HomeComponent {
 
   getThreadMessage() {
     return this.homeService.getThreadMessage();
+  }
+
+  closeThread() {
+    this.homeService.closeThread();
+  }
+
+  toggleNav() {
+    this.homeService.toggleNav();
+  }
+
+  getScreenMode() {
+    return this.homeService.getScreenMode();
+  }
+
+  isNavVisible() {
+    return this.homeService.isNavVisible();
+  }
+
+  isThreadVisible() {
+    return this.homeService.isThreadVisible();
+  }
+
+  hasThreadSpace() {
+    return this.homeService.hasThreadSpace();
+  }
+
+  isChannelVisible() {
+    return this.homeService.isChannelVisible();
   }
 }
--- src/app/services/home.service.ts
+++ src/app/services/home.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { ChannelData, Message } from './firebase-channel.service';
+import { Subscription } from 'rxjs';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { MAX_INLINE_WIDTH, LARGE_WIDTH, MEDIUM_WIDTH, SMALL_WIDTH } from '../../global-constants';
 
 @Injectable({
   providedIn: 'root'
@@ -7,15 +10,111 @@ import { ChannelData, Message } from './firebase-channel.service';
 export class HomeService {
   selectedChannel: ChannelData | undefined;
   selectedMessage: Message | undefined;
+  screenMode: "small" | "medium" | "large" = "large";
+  navVisible = true;
+  threadVisible = true;
+  threadVisibleMq!: boolean;
+  channelVisible!: boolean;
+  private breakpointSubscription!: Subscription;
 
-  constructor() { }
+  constructor(
+    private responsive: BreakpointObserver,) { 
+      this.breakpointSubscription = this.responsive.observe([
+        SMALL_WIDTH,
+        MEDIUM_WIDTH,
+        LARGE_WIDTH
+      ])
+        .subscribe(() => {
+  
+          if (this.responsive.isMatched(SMALL_WIDTH)) {
+            this.screenMode = "small";
+            if (this.navVisible) {
+              this.channelVisible = false;
+              this.threadVisibleMq = false;
+            }
+          } else if (this.responsive.isMatched(MEDIUM_WIDTH)) {
+            this.screenMode = "medium";
+            if (this.navVisible) {
+              this.channelVisible = true;
+              this.threadVisibleMq = false;
+            }
+          } else if (this.responsive.isMatched(LARGE_WIDTH)) {
+            this.screenMode = "large";
+            this.threadVisibleMq = true;
+            this.channelVisible = true;
+          }
+        });
+    }
+
+  ngOnInit() {
+    
+  }
+
+  ngOnDestroy() {
+    this.breakpointSubscription.unsubscribe();
+  }
+
+  toggleNav() {
+    if (this.screenMode == "small") {
+      this.channelVisible = false;
+      this.threadVisibleMq = false;
+    }
+    this.navVisible = !this.navVisible;
+  }
+
+  closeThread() {
+    if (this.screenMode != "large") {
+      this.threadVisibleMq = false;
+    }
+    this.threadVisible = false;
+    this.channelVisible = true;
+  }
+
+  openThread() {
+    this.threadVisibleMq = true;
+    this.threadVisible = true;
+    if (this.screenMode != "large") {
+      this.channelVisible = false;
+    }
+    document.body.style.overflow = "hidden";
+    setTimeout(() => {
+      document.body.removeAttribute("style");
+    }, 500);
+  }
+
+  openChannel() {
+    this.navVisible = false;
+    this.channelVisible = true;
+  }
+
+  getScreenMode() {
+    return this.screenMode;
+  }
+
+  isNavVisible() {
+    return this.navVisible;
+  }
+
+  isThreadVisible() {
+    return this.threadVisible;
+  }
+
+  hasThreadSpace() {
+    return this.threadVisibleMq;
+  }
+
+  isChannelVisible() {
+    return this.channelVisible;
+  }
 
   setChannel(channel: ChannelData) {
+    if (channel !== this.selectedChannel) this.closeThread();
     this.selectedChannel = channel;
   }
 
   setThreadMessage(message: Message) {
     this.selectedMessage = message;
+    this.openThread();
   }
 
   getActiveChannel() {
