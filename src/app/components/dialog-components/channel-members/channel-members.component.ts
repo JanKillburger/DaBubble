77a39eda848c4bd7a87bcca8d66faac7e0253596
Jan Killburger <jkillburger@gmail.com>
2024-03-03 08:22:21 +0100

    completed UI

--- src/app/components/dialog-components/channel-members/channel-members.component.ts
+++ src/app/components/dialog-components/channel-members/channel-members.component.ts
@@ -3,11 +3,13 @@ import { ContactButtonComponent } from '../../contact-button/contact-button.comp
 import { UserData } from '../../../services/firebase-user.service';
 import { NgFor } from '@angular/common';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-channel-members',
   standalone: true,
-  imports: [ContactButtonComponent, NgFor],
+  imports: [ContactButtonComponent, NgFor, MatIconModule, MatButtonModule],
   templateUrl: './channel-members.component.html',
   styleUrl: './channel-members.component.scss'
 })
