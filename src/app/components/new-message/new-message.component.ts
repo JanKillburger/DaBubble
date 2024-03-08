0f2f107d118f657bd975e901f09ad7d39b2b61f9
Jan Killburger <jkillburger@gmail.com>
2024-03-08 08:47:25 +0100

    added basic new message functionality

--- src/app/components/new-message/new-message.component.ts
+++ src/app/components/new-message/new-message.component.ts
@@ -1,14 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { MessagesInputComponent } from '../messages-input/messages-input.component';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { UserData } from '../../services/firebase-user.service';
 import { ChannelData, FirebaseChannelService } from '../../services/firebase-channel.service';
 import { FirebaseAuthService } from '../../services/firebase-auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-message',
   standalone: true,
-  imports: [MessagesInputComponent, NgFor],
+  imports: [MessagesInputComponent, NgFor, NgIf, FormsModule],
   templateUrl: './new-message.component.html',
   styleUrl: './new-message.component.scss'
 })
@@ -17,23 +18,37 @@ export class NewMessageComponent {
   authService = inject(FirebaseAuthService);
   channelSearchResults: ChannelData[] = [];
   userSearchResults: UserData[] = [];
+  query = '';
+  recipient: Recipient | undefined;
 
-  getSearchResults(query: string) {
-    if (query.length === 1) {
+  getSearchResults() {
+    if (this.query.length === 1) {
       this.userSearchResults = [];
       this.channelSearchResults = [];
       return;
     }
-    if (query.startsWith("#")) {
+    if (this.query.startsWith("#")) {
       this.userSearchResults = [];
-      this.channelSearchResults = this.getFirstN(this.channelService.userChannels.filter(channel => channel.channelName.toLowerCase().includes(query.replace('#', '').toLowerCase())), 5);
-    } else if (query.startsWith("@")) {
+      this.channelSearchResults = this.getFirstN(this.channelService.userChannels.filter(channel => channel.channelName.toLowerCase().includes(this.query.replace('#', '').toLowerCase())), 5);
+    } else if (this.query.startsWith("@")) {
       this.channelSearchResults = [];
-      this.userSearchResults = this.getFirstN(this.authService.allUsers.filter(user => user.name.toLowerCase().includes(query.replace('@', '').toLowerCase())), 5);
+      this.userSearchResults = this.getFirstN(this.authService.allUsers.filter(user => user.name.toLowerCase().includes(this.query.replace('@', '').toLowerCase())), 5);
     } else {
       this.userSearchResults = [];
       this.channelSearchResults = [];
     }
+  }
+
+  selectUser(user: UserData) {
+    this.query = "@" + user.name;
+    this.recipient = {type: "user", target: user};
+    this.userSearchResults = [];
+  }
+
+  selectChannel(channel: ChannelData) {
+    this.query = "#" + channel.channelName;
+    this.channelSearchResults = [];
+    this.recipient = {type: "channel", target: channel};
   }
 
   getFirstN(arr: any[], n: number) {
@@ -43,4 +58,10 @@ export class NewMessageComponent {
       return arr.slice(0, n);
     }
   }
+
+  
+}
+interface Recipient {
+    type: "email" | "channel" | "user";
+    target: string | ChannelData | UserData;
 }
